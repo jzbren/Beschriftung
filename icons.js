cb5f799dc3be1ed87c33336b6c2d4437ba11d6c6
jzbren <jzbren@gmail.com>
2026-07-24 06:34:06 +0200

    Add initial icon configuration list in icons.js

---
+++ icons.js
@@ -0,0 +1,42 @@
+// Diese Datei dient als "Textdokument" zur einfachen Erweiterung der Icons.
+// Trage hier einfach neue SVG-Dateinamen ein (in Anführungszeichen und mit Komma getrennt).
+// Achte darauf, dass die Datei genau "icons.js" heisst und im gleichen Ordner liegt.
+
+const iconConfigList = [
+    "", // Leere Option (für keinen Icon)
+    "Abwesend.svg",
+    "Access-Point-Unifi.svg",
+    "Access-Point.svg",
+    "Akku-Laden.svg",
+    "Akku.svg",
+    "Auffuellen.svg",
+    "Auto.svg",
+    "Baum.svg",
+    "Bewegung.svg",
+    "Bluetooth-Aus.svg",
+    "Bluetooth-Ein.svg",
+    "CSV-Datei.svg",
+    "Deckenleuchte.svg",
+    "Download.svg",
+    "Drucken.svg",
+    "Drucker-Aus.svg",
+    "Dublizieren.svg",
+    "Einstellungen.svg",
+    "Entrigeln.svg",
+    "Fan-Aus.svg",
+    "Fan-Langsamer.svg",
+    "Fan-Schneller.svg",
+    "Fan-Stufe-1.svg",
+    "Fan-Stufe-2.svg",
+    "Fan-Stufe-3.svg",
+    "Farbpalette-Aus.svg",
+    "Farbpalette.svg",
+    "Farbwahl.svg",
+    "Fenster-Auf.svg",
+    "Fenster-Zu.svg",
+    "Fierwall-Schild.svg",
+    "Fierwall.svg",
+    "Fisch.svg",
+    "Flamme.svg",
+    "Foerwaertsspuelen-Fein-Doppelt.svg"
+];
