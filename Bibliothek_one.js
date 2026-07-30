29958e33c4999c993c55363b8fba37313c40044b
jzbren <jzbren@gmail.com>
2026-07-30 20:39:31 +0200

    Fix typo in name from 'ree' to 'Tree'

--- Bibliothek_one.js
+++ Bibliothek_one.js
@@ -762,7 +762,7 @@ const hardwareConfigList = [
     },
     { 
         id: "lox_tree_to_air", 
-        name: "ree to Air Bridge", 
+        name: "Tree to Air Bridge", 
         image: "Tree-to-Air.jpg", 
         power: 0.5, 
         fields: ["S.-Nr.", "Geräte-Bez.", "Tree Ast", "Bemerkung"], 
